667766f296698ba5c33bb27202fd85eb54f00aa0
rjiandy <jiandy.ricky@gmail.com>
2018-01-17 17:03:44 +0700

    Add injectable text and icon style

--- src/components/core-ui/IconButton.js
+++ src/components/core-ui/IconButton.js
@@ -8,18 +8,20 @@ type Props = {
     icon: string;
     style?: {[key: string]: any};
     iconStyle?: {[key: string]: any};
+    textStyle?: {[key: string]: any};
 };
 
 export default function IconButton(props: Props) {
-    let {text, icon, style, iconStyle, ...otherProps} = props;
+    let {text, icon, textStyle, style, iconStyle, ...otherProps} = props;
     let composedStyle = {...styles.iconButton, ...style};
-    let composedIconStyle = {...styles.icon, ...iconStyle}
+    let composedIconStyle = {...styles.icon, ...iconStyle};
+    let composedTextStyle = {...styles.text, ...textStyle};
     return (
         <div style={composedStyle} {...otherProps}>
             <FontIcon className="material-icons" style={composedIconStyle}>{icon}</FontIcon>
             {
                 text ? (
-                    <span style={styles.text}>{text}</span>
+                    <span style={composedTextStyle}>{text}</span>
                 ) : null
             }
         </div>
@@ -31,7 +33,7 @@ const styles = {
         display: 'flex',
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center',
+        paddingLeft: '20px',
         color: 'rgba(100, 100, 100, 0.6)',
         cursor: 'pointer',
     },
